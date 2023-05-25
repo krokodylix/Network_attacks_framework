1e75547f05cb08b5a854b3e8383bb2b65ff0aba3
krokodylix <proalo22@gmail.com>
2023-05-25 17:12:22 +0200

    code refactor

--- src/auxiliary/portscanner.py
+++ src/auxiliary/portscanner.py
@@ -28,55 +28,40 @@ def tcpscan(targetip,ports):
 
 
 def synscan(target_ip, ports):
+    openports = []
     for port in ports:
         ip_packet = IP(dst=target_ip)
         tcp_packet = TCP(sport=1234, dport=port, flags="S")
         packet = ip_packet / tcp_packet
         response = sr1(packet, timeout=1, verbose=0)
-
         if response and response.haslayer(TCP):
             if response[TCP].flags == "SA":
-                logging.info(f"Port {port} is open")
-            elif response[TCP].flags == "RA":
-                logging.info(f"Port {port} is closed")
-        else:
-            logging.exception(f"Port {port} is filtered or no response received")
+                openports.append(port)
 
+    return openports
 
 def nullscan(target_ip, ports):
+    openports = []
     for port in ports:
         ip_packet = IP(dst=target_ip)
         tcp_packet = TCP(dport=port, flags="")
         packet = ip_packet / tcp_packet
         response = sr1(packet, verbose=False, timeout=5)
-
-        if response is None:
-            logging.info(f"Port {port} on {target_ip} is open or filtered.")
-        elif response.haslayer(TCP):
-            if response.getlayer(TCP).flags == 0x14:
-                logging.info(f"Port {port} on {target_ip} is closed.")
-            elif response.getlayer(TCP).flags == 0x04:
-                logging.info(f"Port {port} on {target_ip} is open.")
-        else:
-            logging.info(f"Port {port} on {target_ip} could not be reached.")
+        if response is not None and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x04:
+            openports.append(port)
+    return openports
 
 
 def finscan(target_ip, ports):
+    openports = []
     for port in ports:
         ip_packet = IP(dst=target_ip)
         tcp_packet = TCP(dport=port, flags="F")
         packet = ip_packet / tcp_packet
         response = sr1(packet, verbose=False, timeout=5)
-
-        if response is None:
-            logging.info(f"Port {port} on {target_ip} is open or filtered.")
-        elif response.haslayer(TCP):
-            if response.getlayer(TCP).flags == 0x14:
-                logging.info(f"Port {port} on {target_ip} is closed.")
-            elif response.getlayer(TCP).flags == 0x04:
-                logging.info(f"Port {port} on {target_ip} is open.")
-        else:
-            logging.info(f"Port {port} on {target_ip} could not be reached.")
+        if response is not None and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x04:
+            openports.append(port)
+    return openports
 
 
 
--- src/auxiliary/traceroute.py
+++ src/auxiliary/traceroute.py
@@ -3,5 +3,4 @@ import os
 
 
 def traceroute(targetip):
-    os.system(f'tracert {targetip}')
-    #[print(snd.ttl, rcv.src) for snd,rcv in sr(IP(dst=targetip, ttl=(1, 50),id=RandShort()) / TCP(flags=0x2), timeout=3 )[0]]
+    return [(snd.ttl, rcv.src) for snd,rcv in sr(IP(dst=targetip, ttl=(1, 50),id=RandShort()) / TCP(flags=0x2), timeout=3 )[0]]
